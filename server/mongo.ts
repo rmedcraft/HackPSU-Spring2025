e33a0051e286743c9060a58482001348d880a78a
rmedcraft <rwtmedcraft@gmail.com>
2025-03-30 08:52:03 -0400

    mongodb works!!!

---
+++ server/mongo.ts
@@ -0,0 +1,20 @@
+// import mongoose from 'mongoose';
+
+// // Define the resume schema
+// const resumeSchema = new mongoose.Schema({
+//     name: { type: String, required: true }
+//     // education: { type: [String], default: [] },
+//     // experience: { type: [String], default: [] },
+//     // skills: { type: [String], default: [] },
+//     // contact: {
+//     //     type: Object, default: {
+//     //         email: "",
+//     //         phone: ""
+//     //     }
+//     // },
+//     // location: { type: String },
+// });
+
+// const Resume = mongoose.model('Employr', resumeSchema, "Resumes");
+
+// export default Resume;
